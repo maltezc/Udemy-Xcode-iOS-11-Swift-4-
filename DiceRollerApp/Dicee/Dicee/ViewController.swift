4c20f1d450d6322fe9996e3f5d4601c5fa1bd1b9
cflux1990 <cflux.maltez@live.com>
2018-10-18 19:05:03 -0700

    5.43 - Learn about Arrays in Swift and Programmatically Update the Display

--- DiceRollerApp/Dicee/Dicee/ViewController.swift
+++ DiceRollerApp/Dicee/Dicee/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 var randomDiceIndex1 : Int = 0
-var randomDiceIndex2 : Int = 1
+var randomDiceIndex2 : Int = 0
+
+let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
 
 class ViewController: UIViewController {
@@ -29,6 +31,9 @@ class ViewController: UIViewController {
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
         print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named:diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named:diceArray[randomDiceIndex2])
     }
     
 }
