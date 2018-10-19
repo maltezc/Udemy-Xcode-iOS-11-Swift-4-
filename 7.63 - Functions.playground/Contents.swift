4e085d1b69a1ceef3a145f06db8ac12f409dce83
cflux1990 <cflux.maltez@live.com>
2018-10-19 07:01:31 -0700

    7.64 - Swift Functions Part 2 - Functions with Inputs

--- 7.63 - Functions.playground/Contents.swift
+++ 7.63 - Functions.playground/Contents.swift
@@ -1,9 +1,22 @@
-func getMilk() {
-    print("go to the shops")
-    print("buy 2 cartons of milk")
-    print("pay $2")
-    print("come home")
+//func getMilk() {
+//    print("go to the shops")
+//    print("buy 2 cartons of milk")
+//    print("pay $2")
+//    print("come home")
+//
+//}
+
+func getMilk(howManyMilkCartons: Int) {
+        print("go to the shops")
+        print("buy \(howManyMilkCartons) cartons of milk")
+    
+        let priceToPay = howManyMilkCartons * 2
+    
+        print("pay $\(priceToPay)")
+        print("come home")
     
 }
 
-getMilk()
+
+
+getMilk(howManyMilkCartons: 4)
