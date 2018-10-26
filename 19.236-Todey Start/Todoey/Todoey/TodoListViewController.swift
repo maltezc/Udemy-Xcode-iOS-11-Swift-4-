dc9febc659ea8061a375ceb18c934af244adf88b
cflux1990 <cflux.maltez@live.com>
2018-10-26 08:28:14 -0700

    19.236 - 236. Subclassing a UITableViewController

---
+++ 19.236-Todey Start/Todoey/Todoey/TodoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Chris Maltez on 10/26/18.
+//  Copyright © 2018 Christopher Maltez. All rights reserved.
+//
+//Xcode Intellisense V
+//https://stackoverflow.com/questions/6662395/xcode-intellisense-meaning-of-letters-in-colored-boxes-like-f-t-c-m-p-c-k-etc
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["find Mike","buy eggos", "destroy demi"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+
+    }
+
+    //MARK - TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+
+        cell.textLabel?.text = itemArray[indexPath.row]
+
+        return cell
+    }
+}
+
