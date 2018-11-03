e89382236cfa22b4cf83da180dbdd6455a9e98cd
cflux1990 <cflux.maltez@live.com>
2018-11-03 08:46:45 -0700

    21.284-How to Import the Image Recognition Model & Enable Camera Functionality - Added Inception v3 boiler plate

---
+++ 21.284-How to Import the Image Recognition Model & Enable Camera Functionality/SeaFood/SeaFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Chris Maltez on 11/2/18.
+//  Copyright © 2018 Christopher Maltez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
